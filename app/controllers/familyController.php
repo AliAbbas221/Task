b6be26f648ea7af5eea3b4bfa917d97f90f19a32
sourcem7 <mohamadaladlan@yahoo.com>
2023-05-26 15:20:44 +0300

    Merge branch 'master' of https://github.com/AliAbbas221/Task into test1

--- app/controllers/familyController.php
+++ app/controllers/familyController.php
@@ -7,60 +7,16 @@ use App\Models\Family;
 class Familly extends basecontroller{
 
 }
+
+
+=======
 class FamillyController extends basecontroller{
-    public function index()
-    {
-            
-           $results=Family::getall($this->conn);
-          // print_r($results);
-           require 'views/users/index.php';
-    }
-    public function addfamily(){
-        $f1=new Family();
-        $f1->SaveFamily($this->conn);
-        header('Location:'.BATH_BASE);
-        
 
-    }
-    public function editfamily($id){
-        if($_SERVER['REQUEST_METHOD']==='POST'){
-            $f2=Family::getfamilybyid($this->conn,$id);
-            
-            $f2->setfname($_POST['fname']);
-            $f2->setlname($_POST['lname']);
-            $f2->setmname($_POST['mname']);
-            $f2->setfamilycount($_POST['numberfamily']);
-            $f2->setphone($_POST['phone']);
-            $f2->settatus($_POST['status']);
-            $f2->SaveFamily($this->conn);
-            header('Location:'.BATH_BASE);
-    
-        }
-       
-
-        else{
-            $re=Family::getfamilybyid($this->conn,$id);
-            require __DIR__.'/../../view/users/editfamily.php';
-
-        }
-    }
-    public function DeleteFamily($id){
-    $rr=Family::getfamilybyid($this->conn,$id);
-    $rr->DeleteFamily($this->conn);
-    header('Location:'.BATH_BASE);
-
-   }
-   public function searchfamily(){
-    if($_SERVER['REQUEST_METHOD']==='POST'){
-        $f5=new Family();
-        $results=$f5->FamilySearch($this->conn,$_POST['choice']);
-        require 'views/users/familufound.php';
-   
-     
-   }
-   else{
-    require __DIR__.'/../../views/users/searchinplace.php';
-   }
-}}
+ public function index()
+ {
+        $index= new  Family ; 
+        $index->getall($this->conn); 
+ }
+}
 
 ?>
--- app/models/family.php
+++ app/models/family.php
@@ -3,34 +3,31 @@ declare (strict_types=1);
 namespace App\Models;
 use PDO;
 //include ('database.php');
-// require 'databsae.php';
+require 'C:\xampp\htdocs\Task\config\database.php';
 use mysqli;
 session_start();
 abstract class Model{
+    public $db;
     protected $id;
-    public function getId(){
-     return $this->id;
-    }
-    public function setid($d){
-     $this->id=$d;
-    }
+   public function getId(){
+    return $this->id;
+   }
+   public function setid($d){
+    $this->id=$d;
+   }
  
   
-
 
 }
 class Family extends Model{
 
-    
+    protected $searchterm;
 
     protected $fname;
     protected $mname;
-    protected $lname ,$phone ,$location,$status,$familycount;
+    protected $lname ,$phone , $status,$location;
     public function  getfname(){
         return $this->fname;
-    }
-    public function getnumber(){
- return $this->familycount;
     }
     public function getmname(){
         return $this->mname;
@@ -41,23 +38,35 @@ class Family extends Model{
     public function getlname(){
         return $this->lname;
     }
-    public function setfname( $f){
+    public function setfname(String $f){
         $this->fname=$f;
     }
-//     public function setfamilycount($n){
-//   $this->familycount=$n;
-//     }
     public function setmname( String $m){
         $this->mname=$m;
     }
 
-  
-    public function settatus($s){
-$this->status=$s;
+    public function  setsearchterm($chat){
+        $this->searchterm=$chat;
     }
-   
+    public function  getsearchterm(){
+        return $this->searchterm;
+    }
 
-    public function setphone($x)
+
+    public function FamilySearch($searchterm){
+
+        
+        $sql = "SELECT * FROM family WHERE fname LIKE '%$searchterm%'";
+        $a=$this->$db->prepare($sql);
+        $a->execute();
+
+
+
+
+
+    }
+
+    public function setphone( string $x)
     {
         $this->phone=$x; 
     }
@@ -79,93 +88,32 @@ $this->status=$s;
     public function getlocation(){
         return $this->location; 
      }
-     public function setfamilycount( $x){
-        $this->familycount=$x;
-     }
-     public function getfamilycount()
-     {
-        return $this->familycount; 
-     }
-     public static function getall($db)
+     public function getall($db)
     {
-        $result="SELECT * FROM family";
-        $s=$db->prepare($result);
-        $s->execute();
-    $families =array();
-    while($results = $s->fetchAll(PDO::FETCH_ASSOC)){
-      foreach ($results as $row) {
-    $user4 = new Family();
-    $user4->setId($row['id']);
-    $user4->setfname($row['fname']);
-    $user4->setmname($row['mname']);
-    $user4->setlname($row['lname']);
-    $user4->setphone($row['phone']); 
-    $user4->setstatus($row['status']); 
-    $user4->setfamilycount($row['familycount']);
-    $user4->location=$row['location'];}
-    $families=$user4;
-    // set other properties of $user4 here
-}
-    
-        return $families;
+        $result="SELECT * FROM family ";
+        $s=$this->db->prepare($result);
+         $res= $s->execute();
+         $result=$s->fetchAll(PDO::FETCH_ASSOC);
+        $user =new Family();
+        $user->id=$result['id'];
+        $user->fname=$result['fname'];
+        $user->mname=$result['mname'];
+        $user->lname=$result['lname'];
+        $user-> phone=$result['phone']; 
+        $user->status=$result['status']; 
+        $user->location=$result['location'];
+        return $user;
         ///sheeeet
 } 
 
-
-public  function DeleteFamily($db){
+public function DeleteFamily($db){
     $statement="DELETE FROM family WHERE id='$this->id'";
     $exec=$db->prepare($statement);
     $exec->execute();
 
 
 }
-
-public function FamilySearch($db,$location){
-
-        
-    $sql = "SELECT * FROM family WHERE location='$location'";
-    $a=$db->prepare($sql);
-    $a->execute();
-    $result=$a->fetch(PDO::FETCH_ASSOC);
-    return $result;
-
-}
-public function SaveFamily($db)
-{
-    if($this->id){
-        $q2="UPDATE family SET fname='$this->fname',mname='$this->mname',lname='$this->lname' , familycount='$this->familycount', phone='$this->phone' , status='$this->status', location='$this->location' WHERE id='$this->id'";
-        $s=$db->prepare($q2);
-        $s->execute();
-    }
-    else{
-        $q22="INSERT INTO family (fname,mname,lname,familycount,phone,status,location) VALUES('$this->fname','$this->mname','$this->lname','$this->familycount','$this->phone',,'$this->status','$this->location')";
-        $s=$db->prepare($q22);
-        $s->execute();
-        $this->id=$db->lastInsertId();
-        
-    }
-   
-}
-
-public static function getfamilybyid($db,$id){
-        $query="SELECT * FROM family WHERE id='$id'";
-        $s=$db->prepare($query);
-        $s->execute();
-        $result=$s->fetch(PDO::FETCH_ASSOC);
-        $Family1 =new Family();
-        $Family1->fname=$result['fname'];
-        $Family1->mname=$result['mname'];
-        $Family1->lname=$result['lname'];
-        $Family1->phone=$result['phone'];
-        $Family1->status=$result['status'];
-        $Family1->location=$result['location'];
-        $Family1->familycount=$result['familycount'];
-        return $Family1;
-        
-
-} 
-
-//$searchterm;
+     
 
 
 }
